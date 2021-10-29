1ecb96462b5f870181c0f19ac2307f9f3ecc4395
Ajay Elamparithi <ajayaj7095@gmail.com>
2021-10-29 14:31:29 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 function submit(){
-  alert("Successfully Submitted");
-};
+    alert("hey");
+}
 
 
 let nameIn = document.getElementById("name");
@@ -15,9 +16,6 @@ nameIn.onblur = function(){
         } else if (nameIn.value === "") {
             spanName.innerText = "Enter Username";
             spanName.style.color = "red"
-        } else if (nameIn.value.search(/^\S*$/)){
-            spanName.innerText = "Spaces cannot allow";
-            spanName.style.color = "red";
         } else if (nameIn.value.search(/[a-zA-Z]/) === -1){
             spanName.innerText = "Enter Valid Name";
             spanName.style.color = "red";
@@ -31,25 +29,17 @@ nameIn.onblur = function(){
     }
 };
 
+
 let email = document.getElementById("email");
 let spanEmail = document.getElementById("email-span");
 email.onblur = function (){
-    let reEmail = /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{2,20})(.[a-z]{2,8})?$/;
-            if (reEmail.test(email.value) === -1){
-                spanEmail.innerText = "Enter Valid Email Address";  
-                spanEmail.style.color = "red"; 
-            } else if (email.value === ""){
-                spanEmail.innerText = "Enter Email Address";
-                spanEmail.style.color = "red";
-            } else if (email.value.search(/@([a-z0-9-]+)/) === -1){
-                spanEmail.innerText = "Invalid Domain Name";
-                spanEmail.style.color = "red";
-            } else if (email.value.search(/@([a-z0-9-]+)\.([a-z]{2,20})/) === -1){
-                spanEmail.innerText = "Invalid Email Extension";
-                spanEmail.style.color = "red";
-            } else {
+    let reEmail =  /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+            if (email.value.match(reEmail)){
                 spanEmail.innerText = "Email Verified";
-                spanEmail.style.color = "green";
+                spanEmail.style.color = "green"; 
+            } else {
+                spanEmail.innerText = "Invalid Email";
+                spanEmail.style.color = "red";
             }
 }
 
@@ -74,22 +64,50 @@ gender.onblur = function(){
         spanGender.innerText = "Please Select Your Gender";
         spanGender.style.color = "red";
     }
-    //  else if (gender.value === "male"){
-    //     spanGender.innerText = "Character Limit 150";
-    //     spanGender.style.color = "red";
-    // }
     else{
         spanGender.innerText = "Message saved";
         spanGender.style.color = "green";
     }
 }
 
-document.getElementById('btn').addEventListener('click', function(){
-  if(spanName.innerText === "Name Accepted" && spanEmail.innerText === "Email Verified"){
-      alert("Your Message was received successfully.");
-  } else if (spanMessage.innerText === "Message Saved"){
-      alert("Your Message was received successfully.");
-  } else{
-      alert("Recheck Inputs");
-  }
-});
+
+// document.getElementById('btn').addEventListener('click', function(){
+//   if(spanName.innerText === "Name Accepted" && spanEmail.innerText === "Email Verified"){
+//       alert("Your Message was received successfully.");
+//   } else if (spanMessage.innerText === "Message Saved"){
+//       alert("Your Message was received successfully.");
+//   } else{
+//       alert("Recheck Inputs");
+//   }
+// });
+
+// function sendEmail(){
+//     Email.send({
+//         Host: 'smtp.gmail.com',
+//         SecureToken : "41d1170c-4d73-4da2-b687-908d72c5218f",
+//         Port:2525,
+//         EnableSsl : true,
+//         Username : 'blackhathubguyz@gmail.com',
+//         Password : '1D54E78AD1A3F6AFEF05D2D745F8317B8842',
+//         To : 'ajaye995@gmail.com',
+//         From : "blackhathubguyz@gmail.com",
+//         Subject : "Black Hat Hub",
+//         Body : this.nameIn.value +"Welcome to black hat hub guys"
+//     }).then(
+// 		message => alert("mail sent successfully")
+// 	);
+// }
+
+function sendmail(params){
+    var tempParams = {
+        from_name : document.getElementById("name").value,
+        id_email : document.getElementById("email").value,
+       
+    };
+    emailjs.send('service_p8n74kf','template_y4l9fk3',tempParams)
+.then(function(res){
+        console.log("success",res.status)
+        if(res.status!=200)alert("Unable to sent your message");
+        else alert("Your message was sent successfully");
+    })
+}
